3e0cc3f4f7ede398b4d89f7cae072de8b05cc6ca
Andre-Leandro <andreleandrosanlorenzo@gmail.com>
2024-06-01 19:22:21 -0300

    new fix

--- test/test.js
+++ test/test.js
@@ -1,17 +1,17 @@
-import chai from 'chai';
+import { expect } from 'chai';
 
 describe('Suma de Números', () => {
   it('debería sumar correctamente dos números', () => {
     const num1 = 2;
     const num2 = 3;
     const suma = num1 + num2;
-    chai.expect(suma).to.equal(5);
+    expect(suma).to.equal(5);
   });
 
   it('debería retornar NaN si uno de los valores no es un número', () => {
     const num1 = 'abc';
     const num2 = 3;
     const suma = parseFloat(num1) + num2;
-    chai.expect(isNaN(suma)).to.be.true;
+    expect(isNaN(suma)).to.be.true;
   });
 });
