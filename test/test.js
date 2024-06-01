e8a61ef04c8581a8fd436129e48cf3697509e71b
Andre-Leandro <andreleandrosanlorenzo@gmail.com>
2024-06-01 19:16:35 -0300

    fix

--- test/test.js
+++ test/test.js
@@ -1,17 +1,17 @@
-const { expect } = require('chai');
+import chai from 'chai';
 
 describe('Suma de Números', () => {
   it('debería sumar correctamente dos números', () => {
     const num1 = 2;
     const num2 = 3;
     const suma = num1 + num2;
-    expect(suma).to.equal(5);
+    chai.expect(suma).to.equal(5);
   });
 
   it('debería retornar NaN si uno de los valores no es un número', () => {
     const num1 = 'abc';
     const num2 = 3;
     const suma = parseFloat(num1) + num2;
-    expect(isNaN(suma)).to.be.true;
+    chai.expect(isNaN(suma)).to.be.true;
   });
 });
