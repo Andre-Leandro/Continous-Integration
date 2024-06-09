d1613c7f435794bc4572e48ba3f9c0801086b059
Andre-Leandro <andreleandrosanlorenzo@gmail.com>
2024-06-09 13:14:44 -0300

    add cypress

--- script2.js
+++ script2.js
@@ -8,10 +8,3 @@ export default function sum(num1, num2) {
         return 'La suma es: ' + suma;
     }
 }
-
-function sumar() {
-    var num1 = document.getElementById('num1').value;
-    var num2 = document.getElementById('num2').value;
-    var result = sum(num1, num2);
-    document.getElementById('resultado').innerText = result;
-}
