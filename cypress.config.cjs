1a0259409f0c92bbe6e1d82521c36fde37300348
Andre-Leandro <andreleandrosanlorenzo@gmail.com>
2024-06-09 02:43:33 -0300

    cypress configuration

---
+++ cypress.config.cjs
@@ -0,0 +1,14 @@
+// cypress.config.js
+
+module.exports = {
+    e2e: {
+      baseUrl: 'https://continous-integration.onrender.com/',
+      setupNodeEvents(on, config) {
+        // implementa aquí los listeners de eventos de Node
+      },
+      supportFile: false
+    },
+  };
+
+
+
