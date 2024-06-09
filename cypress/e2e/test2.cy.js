9061f9f45f67ced53b3a8352a3d32c082e89983a
Andre-Leandro <andreleandrosanlorenzo@gmail.com>
2024-06-09 18:54:37 -0300

    nueva prueba

---
+++ cypress/e2e/test2.cy.js
@@ -0,0 +1,10 @@
+describe('Page Title Verification', () => {
+    it('should have the correct title', () => {
+      // Visita la URL de la página que quieres probar
+      cy.visit('https://continous-integration.onrender.com');
+  
+      // Verifica que el título de la página sea el esperado
+      cy.title().should('eq', 'Duende');
+    });
+  });
+  
